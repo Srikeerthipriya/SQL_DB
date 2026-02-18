a3529eb77fcbd8c576bd756d552fa3cc5572af25
Srikeerthipriya <srikeerthipriyashankar@gmail.com>
2026-02-18 09:56:43 +0530

    Groupby in sales DB

---
+++ Database_name _praticse/sales_db/groupby_sales.sql
@@ -0,0 +1,57 @@
+-- Groupby 
+
+-- For each city
+-- Max, Min, Total and Average order value, 
+
+-- having -- conditon 
+-- min order quantity more than 2
+
+-- For each city
+SELECT
+	COUNT(REGION),
+	REGION
+FROM
+	SALES
+GROUP BY
+	REGION;  -- 13 city 
+
+-- Max, Min, Total and Average order value, 
+SELECT
+	REGION,
+	MAX(PRICE),
+	MIN(PRICE),
+	SUM(PRICE)
+FROM
+	SALES
+GROUP BY
+	REGION; 
+
+
+-- min order quantity more than 2
+
+-- 13 city 
+SELECT
+	REGION,
+	SUM(QUANTITY)
+FROM
+	SALES
+GROUP BY
+	REGION
+HAVING
+	SUM(QUANTITY) > 2;
+
+ -- min order quantity more than 10
+
+-- 9 city 
+SELECT
+	REGION,
+	SUM(QUANTITY)
+FROM
+	SALES
+GROUP BY
+	REGION
+HAVING
+	SUM(QUANTITY) > 10;
+
+ 
+
