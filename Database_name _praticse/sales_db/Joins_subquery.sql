b66adb789b2a0c02abe1820f065604ae7ca88a63
Srikeerthipriya <srikeerthipriyashankar@gmail.com>
2026-03-06 10:51:43 +0530

    using joins & subquery 1. Customers who have ordered more than twice 2. Customers who have never ordered anything

--- Database_name _praticse/sales_db/Joins_subquery.sql
+++ Database_name _praticse/sales_db/Joins_subquery.sql
@@ -1,38 +1,49 @@
--- 1. Customers who have ordered more than twice
--- 2. Customers who have never ordered anything
-
 select * from customer;
-
 select * from orders;
 
--- 1. Customers who have ordered more than twice
+INSERT INTO Customer (id, fname, lname, email, city, state) VALUES
+(26,'Nikshi','P','Nik24@gmail.com','chennai','tamilnadu'),
+(27,'keerthi','sri','sri24@gmail.com','pondicherry','tamilnadu'),
+(28,'subha','sri','subha@gmail.com','munnar','kerala')
+
+-- 1. Customers who have ordered more than twice 
+
 -- using joins 
-SELECT c.id, c.fname,count(o.order_id)
-FROM customer c
-JOIN Orders o
-ON c.id = o.customer_id
-GROUP BY c.id, c.fname
-HAVING COUNT(o.order_id) > 2;
+select c.id,c.fname,count(o.order_id)
+from customer c
+join orders o
+on c.id = o.customer_id
+group by c.id,c.fname
+having count (o.order_id) >2
 
+-- 15 records 
 
--- using sub query 
+-- subquery 
+-- 15 customer details order more than 2 
+
 select * from customer
-where id in
-(select customer_id
+where id in (
+select customer_id
 from orders 
-group by customer_id having count(order_id )>2);
+group by customer_id 
+having count (order_id) >2))
+
 
 -- 2. Customers who have never ordered anything
 -- using joins 
-select c.id,c.fname
-from customer c 
-left join orders o 
-on c.id=o.customer_id
-where not o.order 
--- group by c.id,c.fname
--- having o.order_id is null 
 
-select * from order 
-where order_id not 
+select c.id, c.fname
+from customer c
+left join orders o 
+on c.id = o.customer_id
+where o.order_id is null
+
+-- using subquery 
+
+select * from customer
+where id not in (
+select customer_id 
+from orders)
+
 
 
